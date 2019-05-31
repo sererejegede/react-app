84a9781921f0259df7223417f2ac6054928d7c5a
Serere Jegede <sererejegede@gmail.com>
2019-05-31 16:48:04 +0100

    Initial commit

---
+++ App.js
@@ -0,0 +1,66 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View, Button} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+
+  buttonPress = () => {
+    console.log('Pressed!');
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.fineButton}> 889372704799 </Text>
+        <Button title="fine button" style={styles.fineButton} onPress={this.buttonPress}/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  fineButton: {
+    width: 150,
+    textAlign: 'center',
+    backgroundColor: '#760AF3',
+    marginTop: 15,
+    marginBottom: 15,
+    padding: 3,
+    color: '#fff',
+    borderRadius: 5
+  }
+});
